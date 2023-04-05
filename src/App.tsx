8db61c0cda3fbfda064ab0c714a5b776049959c0
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-04-04 21:47:23 -0300

    (feature) add basic css

--- src/App.tsx
+++ src/App.tsx
@@ -29,10 +29,29 @@ function App() {
   const [displayError, setDisplayError] = useState(false);
   const [displayCard, setDisplayCard] = useState(false);
   
+  const buscar = (e: { preventDefault: () => void })=>{
+    e.preventDefault();
+    fetchUser()
+  }
+
   function fetchUser(){
+    //resetuser
+    setDisplayUser({
+      login: "",
+      name: "",
+      bio: "",
+      blog: "",
+      public_repos: 0,
+      followers: 0,
+      following: 0,
+      avatar_url: "",
+      html_url: "",
+      repos_url: "",
+      repos: [],    
+    })      
+
     setDisplayError(false);
-    setDisplayCard(false);
-  
+    setDisplayCard(false);  
     axios.get(`${apiUrl}${inputUsername}`).then((res)=>{
       setDisplayError(false);
       setDisplayCard(true);
@@ -82,10 +101,7 @@ function App() {
     } 
   }, [displayUser.repos_url]);
 
-  const buscar = (e: { preventDefault: () => void })=>{
-    e.preventDefault();
-    fetchUser()
-  }
+  
 
   return (
     <div className="App">
@@ -102,47 +118,49 @@ function App() {
       <div className='usercard' style={ displayCard ? {display:"block"} : {display: 'none'}}>        
         <div className='usercard'>
           <h1>{displayUser.name}</h1>
-          <h1>/{displayUser.login}</h1>
-          <h1>{displayUser.bio}</h1>
+          <h3>/{displayUser.login}</h3>
+          <p>{displayUser.bio}</p>
           <a href={`https://${displayUser.blog}`}>{displayUser.blog}</a>
-          <h1>Seguidores: {displayUser.followers}</h1>
-          <h1>Seguindo: {displayUser.following}</h1>
-          <h1>Repositórios: {displayUser.public_repos}</h1>
+          <h4>Seguidores: {displayUser.followers}</h4>
+          <h4>Seguindo: {displayUser.following}</h4>
+          <h4>Repositórios: {displayUser.public_repos}</h4>
           <img src={displayUser.avatar_url}></img>
+
+          
+        <h2>Repositórios</h2>
+        {/* Para verificar se o array de repositórios em displayUser está vazio,
+        você pode usar a função length do array.
+        Se o length for igual a zero, o array está vazio. */}
+        { displayUser.repos.length !== 0 ?
+          (
+            displayUser.repos.map((repo: { name: String,
+              html_url: string,
+              description: string,
+              language: string,
+              startgazers_count: number,
+              forks_count: number,
+              id: number,
+              license: string })=>( 
+              <div key={repo.id}>     
+                <a href={repo.html_url} target='_blank'>{repo.name}</a>    
+                <p>{repo.description}</p> 
+                <h3 style={{color: languageColors[repo.language]}}>{repo.language}</h3> 
+                <div className='langcolor' style={{backgroundColor: languageColors[repo.language], width: 50, height: 50}}></div>
+                <h4>Estrelas: {repo.startgazers_count}</h4>
+                <h5>Forks: {repo.forks_count}</h5>
+                <h3>{repo.license}</h3>
+              </div> 
+            ))         
+          )
+          :
+          (
+            <>
+              <p>Este usuário nao possui repositórios</p>
+            </>          
+          )
+        }
         </div>        
-      </div>      
-      
-      <h2>Repositórios</h2>
-      {/* Para verificar se o array de repositórios em displayUser está vazio,
-      você pode usar a função length do array.
-      Se o length for igual a zero, o array está vazio. */}
-      { displayUser.repos.length !== 0 ?
-        (
-          displayUser.repos.map((repo: { name: String,
-            html_url: string,
-            description: string,
-            language: string,
-            startgazers_count: number,
-            forks_count: number,
-            id: number,
-            license: string })=>( 
-            <div key={repo.id}>     
-              <a href={repo.html_url} target='_blank'>{repo.name}</a>    
-              <p>{repo.description}</p> 
-              <h3 style={{color: languageColors[repo.language]}}>{repo.language}</h3> 
-              <h4>Estrelas: {repo.startgazers_count}</h4>
-              <h5>Forks: {repo.forks_count}</h5>
-              <h3>{repo.license}</h3>
-            </div> 
-          ))         
-        )
-        :
-        (
-          <>
-            <p>Este usuário nao possui repositórios</p>
-          </>          
-        )
-      }
+      </div>  
     </div>
   )
 }
